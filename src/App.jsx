6a1f582b5aa7f043df1830da20df2ea7a91a5074
FacundoECornet <facucor378@gmail.com>
2025-04-22 17:16:28 -0300

    arreglos vercel

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Body from './Componentes/Body.jsx'
 import PropertyList from './Componentes/propiedadesCards.jsx'
 import Nosotros from './Componentes/Nosotros.jsx'
 import Noticias from './Componentes/Noticias.jsx'
-import Footer from './Componentes/footer.jsx' 
+import Footer from './Componentes/footer.jsx';
 import Formulario from './Componentes/Contacto.jsx'
 
 
